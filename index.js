b71241c734b0d2837424a07b3acacfcea45e8be8
karan-ipec22 <64542853+karan-ipec22@users.noreply.github.com>
2021-12-01 01:57:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Mst from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Mst />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
